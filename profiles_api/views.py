df15b8f7f26ee17ae2541258172a9684328fee4f
Yeementh24 <yeementh24@gmail.com>
2022-05-19 13:24:33 +0530

    Added Profiles viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,6 +3,10 @@ from rest_framework.response import Response
 from rest_framework import status
 from profiles_api import serializers
 from rest_framework import viewsets
+from profiles_api import models
+from rest_framework.authentication import TokenAuthentication
+from profiles_api import permissions
+from rest_framework import filters
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -93,3 +97,12 @@ class HelloViewSet(viewsets.ViewSet):
         """Handle removing an object"""
 
         return Response({'http_method': 'DELETE'})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating, creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
